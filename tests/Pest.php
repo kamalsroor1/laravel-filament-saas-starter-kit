db3b796370e8250c0796b721734f34060ce415ef
kamalsroor1 <teko.2031@gmail.com>
2026-05-20 00:49:53 +0300

    test: bootstrap laravel app in pest and enable refresh database

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,3 +2,8 @@
 
 declare(strict_types=1);
 
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class)->in('Feature', 'Unit');
+uses(RefreshDatabase::class)->in('Feature');
